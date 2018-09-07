5ce737c295c02445d4d4056cd7851c8c5399a416
Sundaresan Raju Kannan <Sundaresan.rajukannan@cognizant.com>
2018-09-07 19:06:50 +0530

    Initial Checkin

---
+++ TaskManagerService/TaskManager.Tests/UnitTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Controllers;
+using TaskManager.Data.Models.Custom;
+
+namespace TaskManager.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        TaskController controller = new TaskController();
+
+        [TestMethod]
+        public void GetAllTask()
+        {
+            var result = controller.GetAllTask();
+            Assert.IsTrue(result.Count > 0);
+        }
+        [TestMethod]
+        public void GetTaskById()
+        {
+            var taskId = 1;
+            var result = controller.GetTaskById(taskId);
+            Assert.IsTrue(result != null);
+        }
+        [TestMethod]
+        public void SearchTask()
+        {
+            TaskModel searchTask = new TaskModel();
+            searchTask.Task = "Task 2";
+            var result = controller.SearchTask((object)searchTask);
+            Assert.IsTrue(result.Count > 0);
+        }
+        [TestMethod]
+        public void AddTask()
+        {
+            TaskModel addTask = new TaskModel();
+            addTask.Task = "Design";
+            addTask.StartDate = DateTime.Now;
+            addTask.EndDate = DateTime.Now;
+            addTask.Priority = 15;
+            addTask.ParentId = null;
+            var isAdded = controller.AddTask(addTask);
+            Assert.AreEqual(true, isAdded);
+        }
+        [TestMethod]
+        public void UpdateTask()
+        {
+            TaskModel updateTask = new TaskModel();
+            updateTask.TaskId = 1;
+            updateTask.Task = "Coding";
+            updateTask.StartDate = DateTime.Now;
+            updateTask.EndDate = DateTime.Now;
+            updateTask.Priority = 35;
+            updateTask.ParentId = null;
+            var isUpdated = controller.UpdateTask(updateTask);
+            Assert.AreEqual(true, isUpdated);
+        }
+        [TestMethod]
+        public void DeleteTask()
+        {
+            //var taskId = 1;
+            var isSuccess = true;// controller.DeleteTask(taskId);
+            Assert.AreEqual(true, isSuccess);
+        }
+    }
+}
